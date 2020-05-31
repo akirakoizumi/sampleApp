33ccf0e85e185b5dda4caa65c0543a6b34bb10ef
akirakoizumi <adventureofurban@gmail.com>
2020-06-01 00:10:51 +0900

    テスト変更

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -4,10 +4,6 @@ RSpec.describe 'Products page system spec', type: :system do
     it "responds successfully" do
       true
     end
-
-    xit "responds successfully2" do
-      expect(page.body).to include product.display_price.to_s
-    end
   end
 end
 # end
