f36fc9eaa10ffe059850ac272e6d4416fd635a9b
akirakoizumi <adventureofurban@gmail.com>
2020-06-05 17:35:13 +0900

    userとtaskを関連付け

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'slim-rails'
 
 gem 'bootstrap'
+
+gem 'rails_autolink'
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i(show edit update destroy)
 
   def index
+    @task = current_user.tasks.order('created_at DESC')
     # @q = current_user.tasks.ransack(params[:q])
     # @tasks = @q.result(distinct: true).page(params[:page]).order('created_at DESC')
     #
